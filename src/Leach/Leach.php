df8e77c74f98c61edbdfa5966e0f9d42d42fca5c
Pierre Minnieur <pm@pierre-minnieur.de>
2012-02-14 10:16:10 -0800

    added Leach class (with version information)

---
+++ src/Leach/Leach.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Leach package.
+ *
+ * (c) Pierre Minnieur <pm@pierre-minnieur.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Leach;
+
+/**
+ * @codeCoverageIgnore
+ */
+final class Leach
+{
+    const VERSION = '2.1.0-dev';
+}
